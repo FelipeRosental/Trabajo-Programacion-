03a9e59f80f7e2165d3e889da114c7ff6e767a4e
FelipeRosental <129460222+FelipeRosental@users.noreply.github.com>
2023-04-13 19:39:22 -0300

    Add files via upload

--- backend.py
+++ backend.py
@@ -3,48 +3,7 @@
 from random import *
 from datetime import *
 from string import *
-
-################################ VALIDACIONES DEL PROGRAMA #################################################################
-
-def validacionDNIyTelefono (dni, telefono):
-    while len(dni)==8:
-        if dni.isdigit() == False or telefono.isdigit() == False :
-            return False
-        else:
-            return True
-    return False
-def validacionEdad (edad):
-    if edad.isdigit() == False:
-        return False
-    else:  
-        return True  
-def validacionCodigo (codigo):
-    while len(codigo)==4:
-        if codigo.isdigit() == False:
-            return False
-        else:
-            return True
-    return False
-def validacionFecha (fechareserva, horareserva):
-    if fechareserva != datetime.date or horareserva != datetime.time:
-        return False
-    else:
-        return True
-def validacionStringUsuario (nombre, apellido):
-    if nombre.isalpha() == False or apellido.isalpha() == False:
-        return False
-    else:
-        return True
-def validacionStringCancha (techada, piso, estado):
-    if techada.isalpha() == False or piso.isalpha() == False or estado.isalpha() == False:
-        return False
-    else:
-        return True
-def validacionEmail (email):
-    if "@" not in email:
-        return False
-    else:
-        return True
+import validaciones as val
 
 #######################################         CLASES      ###############################################################
 
@@ -53,47 +12,58 @@ class Usuario ():
         #VALIDACIONES
         self.nombre = input("Ingrese Nombre: ")
         self.apellido = input("Ingrese Apellido: ")
-        while validacionStringUsuario(self.nombre, self.apellido) != True:
+        while val.validacionStringUsuario(self.nombre, self.apellido) != True:
             self.nombre = input("Ingrese Nombre: ")
             self.apellido = input("Ingrese Apellido: ")
         ###    
         self.dni = input("Ingrese DNI: ")
         self.telefono = input("Ingrese Telefono: ")
-        while validacionDNIyTelefono(self.dni, self.telefono) != True:
+        while val.validacionDNIyTelefono(self.dni, self.telefono) != True:
             self.dni = input("Ingrese DNI: ")
             self.telefono = input("Ingrese Telefono: ")
         ###
         self.edad = input("Ingrese Edad: ")    
-        while validacionEdad(self.edad) != True:
+        while val.validacionEdad(self.edad) != True:
             self.edad = input("Ingrese Edad: ")
         ###
         self.email = input("Ingrese Email: ")  
-        while validacionEmail(self.email) != True:
+        while val.validacionEmail(self.email) != True:
            self.email = input("Ingrese Email: ")
-           
+        ###
+        def __str__(self):
+            return "Nombre: " + self.nombre + "\nApellido: " + self.apellido + "\nDNI: " + str(self.dni) + "\nTelefono: " + str(self.telefono) + "\nEdad: " + str(self.edad) + "\nEmail: " + self.email
+        def getDNI(self):
+            return self.dni   
+        def getNombre(self):
+            return self.nombre
 class Cancha (): 
     def __init__(self):
         #VALIDACIONES
         self.codigo = input ("Ingrese Codigo: ")
-        while validacionCodigo (self.codigo) != True:
+        while val.validacionCodigo (self.codigo) != True:
             self.codigo = input ("Ingrese Codigo: ")
         ###    
         self.techada = input ("Ingrese Techada: ")
         self.piso = input ("Ingrese Piso: ")
         self.estado = input ("Ingrese Estado: ")
-        while validacionStringCancha (self.techada, self.piso, self.estado) != True:
+        while val.validacionStringCancha (self.techada, self.piso, self.estado) != True:
             self.techada = input ("Ingrese Techada: ")
             self.piso = input ("Ingrese Piso: ")
             self.estado = input ("Ingrese Estado: ")
+        
+        def __str__(self):
+            return "Codigo: " + str(self.codigo) + "\nTechada: " + self.techada + "\nPiso: " + self.piso + "\nEstado: " + self.estado
                
 class Reserva ():
     def __init__(self):
         #VALIDACIONES
         self.fechareserva = input("Ingrese la fecha de la reserva: ")
         self.horareserva = input("Ingrese la hora de la reserva: ")
-        while validacionFecha(self.fechareserva, self.horareserva):
+        while val.validacionFecha(self.fechareserva, self.horareserva):
             self.fechareserva = input("Ingrese la fecha de la reserva: ")
             self.horareserva = input("Ingrese la hora de la reserva: ")
+        def __str__(self):
+            return "Fecha de la reserva: " + str(self.fechareserva) + "\nHora de la reserva: " + str(self.horareserva)
         
 class Club ():
     
@@ -117,22 +87,25 @@ class Club ():
         self.lista_reservas.append(reserva)
     
     def mostrar_usuarios(self):
-        print("USUARIOS: " + str(self.lista_usuarios))
+        print("USUARIOS:")
+        for user in self.lista_usuarios:
+           print(user)       
     def mostrar_canchas(self):
-        print("CANCHAS: " + str(self.lista_canchas))
-    def mostrar_reservas(self):
-        print("RESERVAS: " + str(self.lista_reservas))
+        print("CANCHAS:")
+        for cancha in self.lista_canchas: 
+            print(cancha)
+    def mostrar_reservas(self):                             
+        print("RESERVAS:")
+        for reserva in self.lista_reservas:
+            print(reserva)
+      
             
-        
 ################################## PRUEBA DE FUNCIONAMIENTO #################################################################
 
-usuario1 = Usuario()
-cancha1 = Cancha()
-club1= Club("Darling")
-club1.agregar_usuarios()
-club1.agregar_canchas()
-club1.mostrar_usuarios()
-club1.mostrar_canchas()
+club1 = Club("Darling")
+Club.agregar_usuarios(club1)
+Club.mostrar_usuarios(club1)
+
 
 
 
